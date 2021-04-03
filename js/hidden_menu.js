fecfdcbd19707eb1fc6132f15d48b558e552ac0c
vvladlena <77752981+vvladlena@users.noreply.github.com>
2021-04-03 15:05:36 +0200

    Add files via upload

---
+++ js/hidden_menu.js
@@ -0,0 +1,38 @@
+const openButton = document.querySelector("#hamburger");
+        const body = document.body;
+        const fullscreenMenu = document.querySelector("#fullscreenMenu");
+
+        const successModal = createModal("The message has been sent, thank you!");
+
+
+        openButton.addEventListener("click", e => {
+            body.appendChild(successModal);
+
+            e.preventDefault();
+
+        });
+
+        function createModal(content) {
+            const overlayElement = document.createElement("div");
+            overlayElement.classList.add("overlay");
+
+
+            const template = document.querySelector("#overlayTemplate");
+
+            overlayElement.innerHTML = template.innerHTML;
+            overlayElement.addEventListener("click", e => {
+                if (e.target == overlayElement) {
+                    closeElement.click();
+                }
+            })
+
+            const closeElement = overlayElement.querySelector("#close");
+            closeElement.addEventListener("click", e => {
+                e.preventDefault();
+                body.removeChild(overlayElement);
+            })
+
+
+            return overlayElement;
+
+        }
